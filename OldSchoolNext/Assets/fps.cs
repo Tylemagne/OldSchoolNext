eee85ecd51858539fa058db005a33667fb2e4c43
Tylemagne <tyler11440_rs2@hotmail.com>
2018-02-26 19:24:05 -0600

    2/2

---
+++ OldSchoolNext/Assets/fps.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fps : MonoBehaviour {
+
+	public Text fpstext;
+	public float lastUpdate;
+
+	// Use this for initialization
+	void Start () {
+		lastUpdate = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > lastUpdate + 0.5f)
+		{
+			int ifps = (int) (1 / Time.deltaTime);
+			fpstext.text = ifps.ToString()+" fps";
+			lastUpdate = Time.time;
+		}
+
+
+		
+	}
+}
---
+++ OldSchoolNext/Assets/init.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(765, 503, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
